106872e1a6c8876f1ef267891b65b51651f8b61e
Nurullah Gundogdu <nurullahgundogdu2304@gmail.com>
2026-07-20 15:54:45 +0300

    test(runner): add goroutine leak test for RunLoadTest using goleak

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nurullahgd/barrage.git
 
 go 1.26.4
+
+require go.uber.org/goleak v1.3.0 // indirect
---
+++ internal/runner/runner_test.go
@@ -0,0 +1,21 @@
+package runner
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"go.uber.org/goleak"
+)
+
+func TestRunLoadTest_NoGoroutineLeak(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	httpClient := &http.Client{}
+	_ = RunLoadTest(httpClient, server.URL, "GET", nil, 50, 5)
+}
